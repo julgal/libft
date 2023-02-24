cee0bc6ad30a104ff3325c7b0a9513c48ac7ad78
Julien Gallais <gallaisjulienlh76@gmail.com>
2023-02-24 19:50:58 +0100

    add ft_strlen

--- libft.h
+++ libft.h
@@ -9,5 +9,8 @@ int	ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
 int	ft_isalnum(int c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *str, int fd);
+size_t	ft_strlen(const char *str);
 
 #endif
--- main.c
+++ main.c
@@ -6,14 +6,18 @@ int	main(void)
 	char	c0;
 	char	c1;
 	char	c2;
+	char	*str0;
 
 	c0 = 'i';
 	c1 = '1';
 	c2 = 10;
+	str0 = "hello";
 	printf("c0 ft_isalpha return : %d\n", ft_isalpha(c0));
 	printf("c0 ft_isascii return : %d\n", ft_isascii(c0));
 	printf("c1 ft_isdigit return : %d\n", ft_isascii(c1));
 	printf("c2 ft_isprint return : %d\n", ft_isprint(c2));
 	printf("c2 ft_isalnum return : %d\n", ft_isalnum(c1));
+	ft_putchar_fd(c1, 1);
+	printf("str0 : %ld caracters\n", ft_strlen(str0));
 	return (0);
 }
