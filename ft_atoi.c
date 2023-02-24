632e7b33c7e4fb3958952c9dbb8aa46ae6143bdb
Julien Gallais <gallaisjulienlh76@gmail.com>
2023-02-24 10:37:19 +0100

    test indentation

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,26 +1,26 @@
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-		int		i;
-		int		res;
-		int		sign;
+	int	i;
+	int	res;
+	int	sign;
 
-		i = 0;
-		res = 0;
-		sign = 1;
-		while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
-			i++ ;
-		if (str[i] == '-' || str[i] == '+')
-		{
-			if (str[i] == '-')
-				sign = -1;
-			i++ ;
-		}
-		while (str[i] >= '0' && str[i] <= '9')
-		{
-			res = (str[i] - 48) + (res * 10);
-			i++ ;
-		}
-		return (res * sign);
+	i = 0;
+	res = 0;
+	sign = 1;
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
+		i++ ;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++ ;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (str[i] - 48) + (res * 10);
+		i++ ;
+	}
+	return (res * sign);
 }
