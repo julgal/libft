03bdb4fbee801a0dd8605f3055a7b190df4479c6
julgal <gallaisjulienlh76@gmail.com>
2023-12-29 14:36:05 +0100

    Adding ft_strcpy and ft_strncpy

--- includes/libft.h
+++ includes/libft.h
@@ -25,6 +25,8 @@ char		*ft_strcat(char *s1, const char *s2);
 char		*ft_strncat(char *s1, const char *s2, size_t n);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
+char		*ft_strcpy(char *dst, const char *src);
+char		*ft_strncpy(char *dst, const char *src, size_t n);
 size_t		ft_strlen(const char *str);
 
 #endif
