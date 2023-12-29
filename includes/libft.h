a8ce2950b1c9859eda8339a6349c86580d01df73
julgal <gallaisjulienlh76@gmail.com>
2023-12-29 14:00:59 +0100

    Adding ft_strchr

--- includes/libft.h
+++ includes/libft.h
@@ -23,6 +23,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char		*ft_strcat(char *s1, const char *s2);
 char		*ft_strncat(char *s1, const char *s2, size_t n);
+char		*ft_strchr(const char *s, int c);
 size_t		ft_strlen(const char *str);
 
 #endif
