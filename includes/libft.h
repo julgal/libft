ee2602d9baab93f72f126622e4ae4338fefcd501
julgal <gallaisjulienlh76@gmail.com>
2024-01-03 21:11:56 +0100

    Adding ft_round

--- includes/libft.h
+++ includes/libft.h
@@ -90,6 +90,7 @@ int		ft_min(int x, int y);
 int		ft_max(int x, int y);
 int		ft_isprime(long unsigned int nbr);
 double		ft_sqrt(double x);
+double		ft_round(double x);
 size_t		ft_fibonacci(unsigned int index);
 size_t		ft_factorial(unsigned int nb);
 
---
+++ srcs/ft_round.c
@@ -0,0 +1,6 @@
+#include "../includes/libft.h"
+
+double	ft_round(double x)
+{
+	return ((int)(x + 0.5));
+}
